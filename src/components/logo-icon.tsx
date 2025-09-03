c4592faedb0a637a04fc03c7578b47e325dd3258
Handy Abi <denny.s4putr4@gmail.com>
2025-09-03 09:04:55 +0000

    gunakan icon berikut pada favicon dan icon aplikasi homescreen mobile :

---
+++ src/components/logo-icon.tsx
@@ -0,0 +1,23 @@
+// Gunakan kode SVG di bawah ini untuk membuat file .png dan .ico Anda.
+// Anda bisa menyalinnya ke konverter online atau alat desain.
+export function LogoIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M7 16.3c2.2 0 4-1.83 4-4.05 0-1.16-.44-2.2-1.2-2.9C8.96 8.55 8 7.21 8 5.5c0-1.4 1.1-2.5 2.5-2.5S13 4.1 13 5.5" />
+      <path d="M17 16.3c-2.2 0-4-1.83-4-4.05 0-1.16.44-2.2 1.2-2.9.8-.7 1.2-1.84 1.2-2.9C15.4 5.1 14.3 4 12.8 4c-1.5 0-2.7 1.1-2.7 2.5" />
+      <path d="M12.2 12.7c.8.7 1.2 1.74 1.2 2.9C13.4 18.13 15.2 20 17.4 20h.6" />
+      <path d="M11.8 12.7c-.8.7-1.2 1.74-1.2 2.9 0 2.22 1.8 4.05 4 4.05h.4" />
+      <path d="M7 16.3c.2.2.5.3.8.3" />
+    </svg>
+  );
+}
